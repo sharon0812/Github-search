591c33af6a762d9772e626950393d091ca823290
sharon0812 <anyangosharon@gmail.com>
2020-11-14 21:14:51 +0300

    add a paragraph

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -8,22 +8,22 @@ import { User } from '../user';
   selector: 'app-search-form',
   templateUrl: './search-form.component.html',
   styleUrls: ['./search-form.component.css']
-})
+})  
 export class SearchFormComponent implements OnInit {
-  @Output() emitSearch = new EventEmitter<any>();
+  
 
 UserName:string;
-  constructor(searchService:SearchService){
-  this.UserName = this.UserName;
-    console.log(this.UserName)
-  }
+
+@Output() emitSearch = new EventEmitter<any>();
+
+  constructor(){}
+  
   get() {
     this.emitSearch.emit(this.UserName);
-    // this.emitSearch.emit(this.repo);  
-    this.UserName;                                                                                                                                                                             
+                                                                                                                                                                                
   }
 
-  ngOnInit(): void {
+  ngOnInit(){
   }
 
 }
