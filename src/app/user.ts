07d187546937b23ddf1f758e33a144c6e6f3aa5c
sharon0812 <anyangosharon@gmail.com>
2020-11-14 21:19:30 +0300

    modify user.ts

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,12 @@
 export class User {
+    constructor(
+        public html_url: any,
+        public name:any,
+        public login:string,
+        public avatar_url:any,
+        public public_repos:number,
+        public created_at:Date,
+        public followers: number,
+        public following: number,
+    ){}
 }
